01ac6f0b54631f029fef8e54a9d83b1b477662ea
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2021-03-11 19:40:35 -0300

    chore: remove @storybook/addon-knobs, @storybook/addon-actions and @storybook/addon-links

--- src/components/main/stories.tsx
+++ src/components/main/stories.tsx
@@ -1,21 +1,21 @@
 import Main from './index'
 // informar meta dados
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { text, withKnobs } from '@storybook/addon-knobs'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
 } as Meta
 
 // render Main component
-export const Basic: Story = () => (
-  <Main
-    title={text('title', 'React Avançado')}
-    description={text(
-      'description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic'
+}
+
+export const Default: Story = (args) => <Main {...args} />
